ef6cc50a768709b2b5ddfacf5887dd26ec0833e3
Jonathan Spira <jjspira@gmail.com>
2024-05-20 22:15:03 -0400

    init

---
+++ src/lib.rs
@@ -0,0 +1,37 @@
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen(module = "/fmodstudioL.js")]
+extern "C" {
+    /// Help
+    fn name() -> String;
+
+    /// Bindings to fmodstudioL
+    type MyClass;
+
+    #[wasm_bindgen(constructor)]
+    fn new() -> MyClass;
+
+    #[wasm_bindgen(method, getter)]
+    fn number(this: &MyClass) -> u32;
+    #[wasm_bindgen(method, setter)]
+    fn set_number(this: &MyClass, number: u32) -> MyClass;
+    #[wasm_bindgen(method)]
+    fn render(this: &MyClass) -> String;
+}
+
+// lifted from the `console_log` example
+#[wasm_bindgen]
+extern "C" {
+    #[wasm_bindgen(js_namespace = console)]
+    fn log(s: &str);
+}
+
+#[wasm_bindgen(start)]
+fn run() {
+    log(&format!("Hello from {}!", name())); // should output "Hello from Rust!"
+
+    let x = MyClass::new();
+    assert_eq!(x.number(), 42);
+    x.set_number(10);
+    log(&x.render());
+}
